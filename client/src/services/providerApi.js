4fbdf302d2d542eccb9132f5f31b1479f9aa94e3
Luis Enrique Gongora Ek <104285100+legongoraek@users.noreply.github.com>
2026-09-14 03:51:54 -0600

    feat: add provider API client

---
+++ client/src/services/providerApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta?.env?.VITE_GARMIN_API_URL || "/api",
+  withCredentials: true,
+});
+
+export async function getProviderReadiness() {
+  const response = await api.get("/providers");
+  return response.data;
+}
+
+export async function startStravaAuthorization() {
+  const response = await api.get("/strava/oauth/start");
+  return response.data;
+}
+
+export async function disconnectStrava() {
+  const response = await api.post("/strava/disconnect");
+  return response.data;
+}
+
+export async function getStravaActivities(params = {}) {
+  const response = await api.get("/strava/activities", { params });
+  return response.data;
+}
+
+export async function getStravaActivityStreams(activityId) {
+  const response = await api.get(`/strava/activities/${encodeURIComponent(activityId)}/streams`);
+  return response.data;
+}
